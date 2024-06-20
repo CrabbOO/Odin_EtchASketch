fcde82f8f6bd4bd0e87e7629a69cee3b1583f7b6
Tema Dubov <tema.dubovv@gmail.com>
2024-06-21 00:05:23 +0300

    finish functionality, refactor later

--- main.js
+++ main.js
@@ -4,10 +4,15 @@ const board = document.querySelector("#board");
 const DEFAULT_GRID_SIZE = 16;
 let size = DEFAULT_GRID_SIZE;
 
+let randomColors = false;
+let eraser = false;
+
 buildBoard(size);
 
 const setGridSize = document.querySelector(".set-size");
 const resetButton = document.querySelector(".reset");
+const eraseButton = document.querySelector(".erase");
+const randomButton = document.querySelector(".random");
 
 // Functions
 function getSize() {
@@ -31,6 +36,7 @@ function buildRow(size) {
     cell.style.height = board.offsetHeight / size + "px";
     cell.style.width = board.offsetWidth / size + "px";
     cell.style.border = "1px solid hsl(0 0% 61% / 21%)";
+    cell.style.backgroundColor = "white";
     row.appendChild(cell);
   }
 
@@ -40,7 +46,30 @@ function buildRow(size) {
 function handleReset() {
   board.innerHTML = "";
   size = getSize();
+  randomColors = false;
+  eraser = false;
   return buildBoard(size);
+}
+
+function colorCell() {
+  let colorMode = eraser
+    ? "white"
+    : randomColors
+      ? `hsl(${Math.random() * 360} 50% 50%)`
+      : "hsl(191 55% 20%)";
+  return colorMode;
+}
+
+function toggleEraser() {
+  eraser = !eraser;
+  randomColors = false;
+  return null;
+}
+
+function toggleRandom() {
+  randomColors = !randomColors;
+  eraser = false;
+  return null;
 }
 
 // Event Listeners
@@ -48,12 +77,17 @@ sizeControl.addEventListener("input", () => {
   sizeLabel.textContent = `${getSize()}x${getSize()}`;
   handleReset();
 });
+
 board.addEventListener("mouseover", (e) => {
   if (e.target.id != "board") {
-    e.target.style.backgroundColor = "hsl(191 55% 20%)";
+    e.target.style.backgroundColor = colorCell();
   }
 });
+
 resetButton.addEventListener("click", handleReset);
+eraseButton.addEventListener("click", toggleEraser);
+randomButton.addEventListener("click", toggleRandom);
+
 document.addEventListener("DOMContentLoaded", () => {
   sizeControl.value = 16;
   sizeLabel.textContent = `${getSize()}x${getSize()}`;
