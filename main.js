c7833b050580be52f3940db49721b8d17a95c31c
Tema Dubov <tema.dubovv@gmail.com>
2024-06-20 22:38:03 +0300

    style elements

--- main.js
+++ main.js
@@ -1,60 +1,60 @@
-const sizeControl = document.querySelector("#size-control")
-const sizeLabel = document.querySelector(".size-label")
+const sizeControl = document.querySelector("#size-control");
+const sizeLabel = document.querySelector(".size-label");
 const board = document.querySelector("#board");
-let size = getSize();
+const DEFAULT_GRID_SIZE = 16;
+let size = DEFAULT_GRID_SIZE;
 
-buildBoard(size)
+buildBoard(size);
 
 const setGridSize = document.querySelector(".set-size");
 const resetButton = document.querySelector(".reset");
 
 // Functions
 function getSize() {
-  return sizeControl.value
+  return sizeControl.value;
 }
 
 function buildBoard(size) {
   for (let i = 0; i < size; i++) {
     const row = buildRow(size);
-    board.appendChild(row)
+    board.appendChild(row);
   }
 }
 
 function buildRow(size) {
-  const row = document.createElement('div');
+  const row = document.createElement("div");
   row.classList.add("row");
 
   for (let i = 0; i < size; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    cell.style.height = (board.offsetHeight / size) + "px";
-    cell.style.width = (board.offsetWidth / size) + "px";
-    cell.style.border = "1px solid limegreen";
+    cell.style.height = board.offsetHeight / size + "px";
+    cell.style.width = board.offsetWidth / size + "px";
+    cell.style.border = "1px solid hsl(0 0% 61% / 21%)";
     row.appendChild(cell);
   }
 
-  return row
+  return row;
 }
 
 function handleReset() {
   board.innerHTML = "";
   size = getSize();
-  return buildBoard(size)
+  return buildBoard(size);
 }
-
 
 // Event Listeners
 sizeControl.addEventListener("input", () => {
   sizeLabel.textContent = `${getSize()}x${getSize()}`;
-})
+  handleReset();
+});
 board.addEventListener("mouseover", (e) => {
   if (e.target.id != "board") {
-    e.target.style.backgroundColor = 'blue';
+    e.target.style.backgroundColor = "hsl(191 55% 20%)";
   }
-})
-resetButton.addEventListener("click", handleReset)
+});
+resetButton.addEventListener("click", handleReset);
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM Loaded")
+  sizeControl.value = 16;
   sizeLabel.textContent = `${getSize()}x${getSize()}`;
-})
-
+});
